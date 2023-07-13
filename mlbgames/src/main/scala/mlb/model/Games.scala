da0fd0132a721be3fc115953d09344fab0b34038
Simon <73712759+ZvSimon@users.noreply.github.com>
2023-07-14 01:29:57 +0300

    Update Games.scala :  Added Games model and type class instances for JDBC and JSON serialization/deserialization.
     These instances include the implicit Schema[Games] instance for defining the database schema, JdbcDecoder[Games] and JdbcEncoder[Games] instances for encoding and decoding Games instances using JDBC, and JsonEncoder[Games] and JsonDecoder[Games] instances for encoding and decoding Games instances to and from JSON. Additionally, a JsonCodec[Games] instance is provided, combining the functionality of encoding and decoding for JSON representations.
    
    These type class instances enable seamless integration of the Games model class with JDBC operations and JSON serialization/deserialization, providing convenient and efficient handling of game data in MLB applications.
    
    The capabilities by introducing the Games model and the corresponding type class instances, facilitating database operations and interoperability with JSON formats.

--- mlbgames/src/main/scala/mlb/model/Games.scala
+++ mlbgames/src/main/scala/mlb/model/Games.scala
@@ -1,8 +1,28 @@
 package mlb.model
 
+import zio.jdbc.*
+import zio.json.*
+import zio.schema.Schema.Field
+import zio.schema.{Schema, TypeId}
+
 final case class Games(
                        date: String,
                        season_year: String,
                        playoff_round: String
                      )
 
+object Games {
+  implicit val schema: Schema[Games] =
+    Schema.CaseClass3[String, String, String, Games](
+      TypeId.parse(classOf[Games].getName),
+      Field("date", Schema[String], get0 = _.date, set0 = (x, v) => x.copy(date = v)),
+      Field("season_year", Schema[String], get0 = _.season_year, set0 = (x, v) => x.copy(season_year = v)),
+      Field("playoff_round", Schema[String], get0 = _.playoff_round, set0 = (x, v) => x.copy(playoff_round = v)),
+      Games.apply
+    )
+  implicit val jdbcDecoder: JdbcDecoder[Games] = JdbcDecoder.fromSchema
+  implicit val jdbcEncoder: JdbcEncoder[Games] = JdbcEncoder.fromSchema
+  implicit val gameEncoder: JsonEncoder[Games] = DeriveJsonEncoder.gen[Games]
+  implicit val gameDecoder: JsonDecoder[Games] = DeriveJsonDecoder.gen[Games]
+  implicit val codec: JsonCodec[Games] = DeriveJsonCodec.gen[Games]
+}
