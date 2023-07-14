2be735a0392e39e701856d2135be87093d709967
Simon <73712759+ZvSimon@users.noreply.github.com>
2023-07-14 23:13:14 +0300

    Update MlbGamesTests.scala
    Added new tests for Method.GET

--- mlbgames/src/test/scala/mlb/MlbGamesTests.scala
+++ mlbgames/src/test/scala/mlb/MlbGamesTests.scala
@@ -2,26 +2,43 @@ package mlb
 
 import munit.*
 import zio.http.*
+import zio.jdbc.ZConnectionPool
 
 class MlbGamesTests extends munit.ZSuite {
 
   val appForStatic: App[Any] = mlb.MlbGames.static
-
+  val appForEndpoints: App[ZConnectionPool] = mlb.MlbGames.endpoints
+  
   testZ("should be Success /text") {
-
     val req = Request.get(URL(Root / "text"))
     assertZ(appForStatic.runZIO(req).isSuccess)
   }
 
   testZ("should be Success /json") {
-
-    val req = Request.get(URL(Root / "text"))
+    val req = Request.get(URL(Root / "json"))
     assertZ(appForStatic.runZIO(req).isSuccess)
   }
 
+  testZ("should be able to run Successfully Root/count") {
+    val req = Request.get(URL(Root / "count"))
+    assertZ(appForEndpoints.runHandler(req).isSuccess)
+  }
+  
   testZ("should be Fail") {
     val req = Request.get(URL(Root))
     assertZ(appForStatic.runZIO(req).isFailure)
   }
-  
+
+  val team1 = "ATL"
+  val team2 = "NYM"
+  testZ("should be able to run Successfully Root/predict/team1/team2") {
+    val req = Request.get(URL(Root / "predict" / team1 / team2))
+    assertZ(appForEndpoints.runHandler(req).isSuccess)
+  }
+
+  testZ("should be able to run Successfully Root/history/team1/team2") {
+    val req = Request.get(URL(Root / "history" / team1 / team2))
+    assertZ(appForEndpoints.runHandler(req).isSuccess)
+  }
+
 }
